8fa27bfe211701fe040e6a58eb914eb8fbba08e4
Ryan <shiing989@gmail.com>
2025-04-16 10:23:27 +0800

    Update challenge b to support input/output file path

--- challenge_b.py
+++ challenge_b.py
@@ -1,7 +1,9 @@
+import sys
 import re
 
 # Constants
-DEFAULT_INPUT_FILE_PATH = "output_data.txt"
+DEFAULT_INPUT_FILE_PATH = "challenge_a_output.txt"
+DEFAULT_OUTPUT_FILE_PATH = "challenge_b_output.txt"
 INTEGER_REGEX_PATTERN = r'-?\d+'        # Regex pattern to match integers
 # Regex pattern to match real numbers, including decimals and scientific notation
 REAL_NUMBER_REGEX_PATTERN = r"""
@@ -48,8 +50,10 @@ def identify_type(obj):
     return "unknown"
 
 
-# Read the input file
-input_file = DEFAULT_INPUT_FILE_PATH
+# Input file path
+input_file = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_INPUT_FILE_PATH
+# Output file path
+output_file = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_OUTPUT_FILE_PATH
 
 with open(input_file, "r") as f:
     data = f.read().strip(",")
@@ -58,7 +62,9 @@ with open(input_file, "r") as f:
 items = data.split(",")
 
 # Analyze each item and print its type
-for item in items:
-    obj = item.strip()  # Remove leading/trailing whitespace
-    obj_type = identify_type(obj)
-    print(f"{repr(obj)} => {obj_type}\n")
+with open(output_file, "w") as f:
+    for item in items:
+        obj = item.strip()  # Remove leading/trailing whitespace
+        obj_type = identify_type(obj)
+        output_line = f"{repr(obj)} => {obj_type}\n"
+        f.write(output_line)
