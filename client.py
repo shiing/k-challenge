1552ccfa300f0cd31d662dfae657d750a6532391
Ryan <shiing989@gmail.com>
2025-05-14 23:55:46 +0800

    Add code logic to detect the concate data. Make sure to split it before saving. Update the data_validator to detect the binary file length correctly

--- client.py
+++ client.py
@@ -30,7 +30,7 @@ def init_db():
 def insert_msg_binary_table(conn, filename):
     cursor = conn.cursor()
 
-    print(f"sql command: INSERT INTO msg_binary (filename) VALUES (\'{filename}\')")
+    # print(f"sql command: INSERT INTO msg_binary (filename) VALUES (\'{filename}\')")
 
     cursor.execute("INSERT INTO msg_binary (filename) VALUES (?)", (filename,))
     conn.commit()
@@ -38,12 +38,45 @@ def insert_msg_binary_table(conn, filename):
 def insert_msg_ascii_table(conn, payload):
     cursor = conn.cursor()    
 
-    print(f"sql command: INSERT INTO msg_ascii (payload) VALUES (\'{payload}\')")
+    # print(f"sql command: INSERT INTO msg_ascii (payload) VALUES (\'{payload}\')")
 
     cursor.execute("INSERT INTO msg_ascii (payload) VALUES (?)", (payload,))
     conn.commit()
 
-def save_bytes_data(data, sock):
+def parse_ascii_data(data, sock):
+
+    end_index = None
+    remaining_buffer = None
+    
+    while True:
+        payload = sock.recv(6)
+
+        try:
+            end_index = payload.decode('ascii').find(";")
+        except UnicodeDecodeError:
+            for i in range(0, 6):
+                try:
+                    end_index = payload[i].decode('ascii').find(";")
+                except UnicodeDecodeError:
+                    continue
+        
+        if end_index == -1:
+            data += payload
+            continue
+
+        data += payload[:end_index + 1]
+        remaining_buffer = payload[end_index + 1:]
+        # print(f"parse_ascii_data: {data}")
+        # print(f"remaining_buffer: {remaining_buffer}")
+        break
+
+    return data, remaining_buffer
+    
+
+def parse_bytes_data(data, sock):
+    remaining_buffer = None
+
+    current_data_type = None
     # Parse header, 1 byte
     header = data[0]
     print(f"Header: 0x{header:02X}")
@@ -54,8 +87,8 @@ def save_bytes_data(data, sock):
     print(f"Payload Size: {payload_size} bytes")
 
     # Parse payload
-    payload = data[6:]
-    payload_count = len(payload)
+    payload = data
+    payload_count = len(payload) - 6
 
     # Use timestamp to create filename
     ts = time.time()
@@ -66,30 +99,54 @@ def save_bytes_data(data, sock):
         f.write(payload)
 
         while payload_count < payload_size:
-            payload = sock.recv(1024)
+            payload = sock.recv(6)
             payload_count += len(payload)
-            f.write(payload)
 
-    return f"{constant.BINARY_FILE_PATH}/{ts}.txt"
+            if payload_count >= payload_size:
+                payload_offset = 6 - (payload_count - payload_size)
+                print(f"payload count: {payload_count}")
+                print(f"payload size: {payload_size}")
+                print(f"payload: {payload}")
+                f.write(payload[:payload_offset])
+                print(f"payload write: {payload[:payload_offset]}")
+                remaining_buffer = payload[payload_offset:]
+                print(f"remaining_buffer: {remaining_buffer}")
+            else:
+                f.write(payload)
+
+    return f"{constant.BINARY_FILE_PATH}/{ts}.txt", remaining_buffer
 
 
 def receive_messages(sock, db_conn):
+    current_buffer = None
+    is_binary = None
     while True:
+        current_data_type = None
         data = None
-        try:
-            data = sock.recv(1024)
-            if not data:
-                print("Server closed the connection.")
-                break
 
-            message = data.decode('utf-8').strip()
-            print(f"Ascii data")
-            insert_msg_ascii_table(db_conn, message)
-            
-        except Exception as e:
-            print(f"Binary data")
-            file_name = save_bytes_data(data, sock)
-            insert_msg_binary_table(db_conn, file_name)
+        if current_data_type is None:
+            data = sock.recv(6)
+        elif current_buffer is not None and len(current_buffer) > 0:
+            data = current_buffer
+
+        print(f"Data: {data}")
+
+        try:
+            data.decode('ascii')
+            is_binary = False
+        except UnicodeDecodeError:
+            is_binary = True
+
+        if is_binary == False and data.decode('ascii')[0] == '$':
+            current_data_type = "ascii"
+            ascii_data, current_buffer = parse_ascii_data(data, sock)
+            insert_msg_ascii_table(db_conn, ascii_data)
+        else:
+            current_data_type = "binary"
+            filename, current_buffer = parse_bytes_data(data, sock)
+            insert_msg_binary_table(db_conn, filename)
+
+                
 
 
 def run_client():
